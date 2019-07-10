a428ec6fe2b67da2f16c59633f78e254d8c42419
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-07-10 16:16:01 -0500

    init git

---
+++ src/main/java/com/charlesbadger/bowlingapi/BowlingApiApplication.java
@@ -0,0 +1,13 @@
+package com.charlesbadger.bowlingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BowlingApiApplication.class, args);
+	}
+
+}
